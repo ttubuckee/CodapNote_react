5c622de10dce2a5c4b226d06e945ab2f3db80672
ttubuckee <tommy1003@naver.com>
2021-02-08 00:30:10 +0900

    Simplify NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,12 +8,10 @@ type NavProps = {
 const NavBar = ({list, onClickTabHandler}: NavProps) => {
   return (
     <div id="navBar">
-      <NavElement index={0} id={`nav${0}`} className='nav-element current-tab' tabTitle={list[0]} onClickTabHandler={onClickTabHandler}/>
-      <NavElement index={1} id={`nav${1}`} className='nav-element' tabTitle={list[1]} onClickTabHandler={onClickTabHandler}/>
-      <NavElement index={2} id={`nav${2}`} className='nav-element' tabTitle={list[2]} onClickTabHandler={onClickTabHandler}/>
-      {/* {list.map((tabTitle,index)=>{
+      {list.map((tabTitle,index)=>{
+        if(index === 0) return <NavElement index={index} id={`nav${index}`} className='nav-element current-tab' tabTitle={tabTitle} onClickTabHandler={onClickTabHandler}/>
         return <NavElement index={index} id={`nav${index}`} className='nav-element' tabTitle={tabTitle} onClickTabHandler={onClickTabHandler}/>
-      })} */}
+      })}
     </div>
   )
 }
